2835eb2dca2b0f590ecd95de2eb11a8dda06e1f5
Sam Elliott <soe1@uakron.edu>
2024-02-29 14:38:42 -0500

    tweaking some of the linting settings

--- button-project/.eslintrc.cjs
+++ button-project/.eslintrc.cjs
@@ -37,5 +37,11 @@ module.exports = {
     rules: {
         "react/react-in-jsx-scope": "off",
         "react/jsx-uses-react": "off",
+        "prettier/prettier": [
+            "error",
+            {
+                "endOfLine": "auto"
+            }
+        ]
     },
 }
--- button-project/src/tests/App.test.tsx
+++ button-project/src/tests/App.test.tsx
@@ -1,6 +1,6 @@
 // Imports
 import { describe, it, expect, afterEach } from 'vitest';
-import '@testing-library/jest-dom/vitest'
+import '@testing-library/jest-dom/vitest';
 import { render, screen, cleanup } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 
@@ -9,55 +9,55 @@ import App from '../App';
 
 // Tests
 describe('Renders main page correctly', async () => {
-    /**
-     * Resets all renders after each test
-     */
-    afterEach(() => {
-        cleanup();
-    });
+  /**
+   * Resets all renders after each test
+   */
+  afterEach(() => {
+    cleanup();
+  });
 
-    /**
-     * Passes - shows title correctly
-     */
-    it('Should render the page correctly', async () => {
-        // Setup
-        await render(<App />);
-        const h1 = await screen.queryByText('Vite + React');
+  /**
+   * Passes - shows title correctly
+   */
+  it('Should render the page correctly', async () => {
+    // Setup
+    await render(<App />);
+    const h1 = await screen.queryByText('Vite + React');
 
-        // Post Expectations
-        expect(h1).toBeInTheDocument();
-    });
+    // Post Expectations
+    expect(h1).toBeInTheDocument();
+  });
 
-    /**
-     * Passes - shows the button count correctly present
-     */
-    it('Should show the button count set to 0', async () => {
-        // Setup
-        await render(<App />);
-        const button = await screen.queryByText('count is 0');
+  /**
+   * Passes - shows the button count correctly present
+   */
+  it('Should show the button count set to 0', async () => {
+    // Setup
+    await render(<App />);
+    const button = await screen.queryByText('count is 0');
 
-        // Expectations
-        expect(button).toBeInTheDocument();
-    });
+    // Expectations
+    expect(button).toBeInTheDocument();
+  });
 
-    /**
-     * Passes - clicks the button 3 times and shows the correct count
-     */
-    it('Should show the button count set to 3', async () => {
-        // Setup
-        const user = userEvent.setup();
-        await render(<App />);
-        const button = await screen.queryByText('count is 0');
+  /**
+   * Passes - clicks the button 3 times and shows the correct count
+   */
+  it('Should show the button count set to 3', async () => {
+    // Setup
+    const user = userEvent.setup();
+    await render(<App />);
+    const button = await screen.queryByText('count is 0');
 
-        // Pre Expectations
-        expect(button).toBeInTheDocument();
+    // Pre Expectations
+    expect(button).toBeInTheDocument();
 
-        // Actions
-        await user.click(button as HTMLElement);
-        await user.click(button as HTMLElement);
-        await user.click(button as HTMLElement);
+    // Actions
+    await user.click(button as HTMLElement);
+    await user.click(button as HTMLElement);
+    await user.click(button as HTMLElement);
 
-        // Post Expectations
-        expect(button?.innerHTML).toBe('count is 3');
-    });
+    // Post Expectations
+    expect(button?.innerHTML).toBe('count is 3');
+  });
 });
